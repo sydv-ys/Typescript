496e93bd4c4c96ff934eb33d0aa57694e3324201
sydv-ys <Aysu.Seyidova@Kibrit.Tech>
2025-04-27 21:08:53 +0400

    setup

---
+++ src/tutorial.ts
@@ -0,0 +1,7 @@
+let orderStatus: "processing" | "shipped" | "delivered" = "processing";
+
+orderStatus = "delivered";
+orderStatus = "shipped";
+
+let discount: number | string = 20;
+discount = "20%";
